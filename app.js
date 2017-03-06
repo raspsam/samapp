2c9c83675ddfc23950dfae95432846bf033da616
Sam <sbgranderson@gmail.com>
2017-03-05 19:14:04 -0600

    fix again

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route EACH PAGE
 app.use('/', index);
 app.use('/users', users);
+app.use('/sam', sam);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
