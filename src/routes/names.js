430a017428a22444a87946b2a29944db1ad6c468
Sam <sbgranderson@gmail.com>
2017-03-26 17:12:35 -0500

    make names page

---
+++ src/routes/names.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var _info = require('./_info.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', _info.person);
+});
+
+module.exports = router;
